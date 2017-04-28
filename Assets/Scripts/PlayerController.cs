59d48b3dd9bcbef4f6f2f575e10727530bbd22f0
Chris <climbrick1989@gmail.com>
2017-04-28 14:43:44 -0400

    Half way in Lecture 106

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed= 15.0f;
 
+	float xmin = -5;
+	float xmax = 5;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -18,9 +21,14 @@ public class PlayerController : MonoBehaviour {
 
 	void ShipControl() {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
+
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		// restrict the player to gamespace
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 }
