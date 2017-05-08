3140194c238683debf0ea6008b2a2d17b8ec5289
Chris Limbrick <climbrick1989@gmail.com>
2017-05-08 06:42:33 -0400

    Lecture 6 Lesson 111

---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed= 15.0f;
+	public float padding = 1f;
+	public GameObject projectile;
+	public float projectileSpeed;
+	public float firingRate = 0.2f;
+
+	float xmin;
+	float xmax;
+
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+
+	void Fire() {
+		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+			beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShipControl();
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			InvokeRepeating("Fire", 0.00001f, firingRate);
+		}
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			CancelInvoke("Fire");	
+		}
+	}
+
+	void ShipControl() {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+		// restrict the player to gamespace
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+	}
+}
