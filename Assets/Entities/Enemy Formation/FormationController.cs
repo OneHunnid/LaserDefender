981b494f991ec52b5d087e28ca6fdb17b117f02f
Chris Limbrick <climbrick1989@gmail.com>
2017-05-10 20:37:54 -0400

    Finish Lesson 116

--- Assets/Entities/Enemy Formation/FormationController.cs
+++ Assets/Entities/Enemy Formation/FormationController.cs
@@ -6,7 +6,8 @@ public class FormationController : MonoBehaviour {
 	public GameObject enemyPrefab;
 	public float width = 10f;
 	public float height = 5f;
-	public float speed = 5;
+	public float speed = 5f;
+	public float spawnDelay = 0.5f;
 
 	private bool movingRight = false;
 	private float xmax;
@@ -21,13 +22,25 @@ public class FormationController : MonoBehaviour {
 		xmax = rightBoundary.x;
 		xmin = leftBoundary.x;
 
-		SpawnEnemies();
+		SpawnUntilFull();
 	}
 
 	void SpawnEnemies() {
 		foreach (Transform child in transform) {
 			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
+		}
+	}
+
+	void SpawnUntilFull() {
+		Transform freePosition = NextFreePosition();
+		if (freePosition) {
+			GameObject enemy = Instantiate (enemyPrefab, freePosition.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = freePosition;
+		}
+
+		if (NextFreePosition()) {
+			Invoke("SpawnUntilFull", spawnDelay);
 		}
 	}
 
@@ -52,8 +65,17 @@ public class FormationController : MonoBehaviour {
 		}
 
 		if (AllMembersDead()) {
-			SpawnEnemies();
+			SpawnUntilFull();
 		}
+	}
+
+	Transform NextFreePosition() {
+		foreach(Transform childPositionGameObject in transform) {
+			if (childPositionGameObject.childCount == 0) {
+				return childPositionGameObject;
+			}
+		}
+		return null;
 	}
 
 	bool AllMembersDead() {
